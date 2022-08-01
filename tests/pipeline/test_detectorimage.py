fa51e47cefeba57913b17c5169b2a01afb5b0552
山根英次 <eijiyamane@gmail.com>
2022-08-01 22:11:57 +0900

    The point totalization must be correct.

--- tests/pipeline/test_detectorimage.py
+++ tests/pipeline/test_detectorimage.py
@@ -34,6 +34,31 @@ def test_makeImage(monkeypatch):
                 assert (sut[x][y] == 0)
 
 
+@pytest.mark.parametrize('pos_array, actual_count', [
+    ([OnDetectorPosition(1, 50., 50., 3000.)], 3000),
+    ([OnDetectorPosition(1, 1000., 1000., 3000.)], 0),
+    ([OnDetectorPosition(1, 50., 50., 4000.), OnDetectorPosition(1, 30., 30., 2000.), OnDetectorPosition(1, 1000., 1000., 3000.)], 6000),
+    ([], 0),
+    ([OnDetectorPosition(1, 50., 50., 0.)], 0),
+])
+def test_makeImage_point_count(monkeypatch, pos_array, actual_count):
+    monkeypatch.setattr(np.random, 'uniform', lambda x, y, z: np.zeros(z))
+    w = WCS(naxis=2)
+    s1 = StellarImage(w)
+    s1.add_position(pos_array)
+    d1 = DetectorImage("outputa.fits")
+    s1.add_child(d1)
+    vs = Simulation()
+    s1.accept(vs)
+
+    sut = d1.get_array()
+    sut_count = 0
+    for row in sut:
+        for col in row:
+            sut_count += col
+    assert(sut_count == actual_count)
+
+
 @pytest.mark.parametrize('pos, randn, actual', [
     (OnDetectorPosition(1, 50., 50., 3000.), (0.0, 0.0), (50, 50)),
     (OnDetectorPosition(1, 30., 40., 3000.), (0.0, 0.0), (40, 30)),  # swap position
