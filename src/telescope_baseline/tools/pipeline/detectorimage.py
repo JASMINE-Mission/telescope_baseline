32d3c6d4b013f671ed122e38d90e082cec7be519
山根英次 <eijiyamane@gmail.com>
2022-08-01 21:41:11 +0900

    I went out and intensively tested the logic to calculate the positions to add points.

--- src/telescope_baseline/tools/pipeline/detectorimage.py
+++ src/telescope_baseline/tools/pipeline/detectorimage.py
@@ -46,10 +46,14 @@ class DetectorImage(SimComponent):
         self.__array = np.random.uniform(0.0, 10.0, (self.__nx, self.__ny))
         for s in self.get_parent_list():
             for j in range(int(s.mag)):
-                xp = int(self.__psf_w * np.random.randn() + s.y + 0.5)
-                yp = int(self.__psf_w * np.random.randn() + s.x + 0.5)
-                if 0 <= xp < self.__nx and 0 <= yp < self.__ny:
-                    self.__array[xp][yp] += 1
+                pos = self._incriment_position(s)
+                if 0 <= pos[0] < self.__nx and 0 <= pos[1] < self.__ny:
+                    self.__array[pos[0]][pos[1]] += 1
+
+    def _incriment_position(self, s):
+        xp = int(self.__psf_w * np.random.randn() + s.y + 0.5)
+        yp = int(self.__psf_w * np.random.randn() + s.x + 0.5)
+        return xp, yp
 
     def load(self):
         """ load fits data
--- tests/pipeline/test_detectorimage.py
+++ tests/pipeline/test_detectorimage.py
@@ -1,14 +1,17 @@
+import pytest
+from pytest_mock import MockFixture
+
 import numpy as np
 from astropy.wcs import WCS
-
 from telescope_baseline.tools.pipeline.detectorimage import DetectorImage
 from telescope_baseline.tools.pipeline.simulation import Simulation
 from telescope_baseline.tools.pipeline.stellarimage import StellarImage, OnDetectorPosition
 
+
 # Test code that continues to maintain what you didn't break by refactoring.
-def test_makeImage(monkeypatch) :
-    monkeypatch.setattr(np.random, 'rand', lambda : 0.5)
-    monkeypatch.setattr(np.random, 'randn', lambda : 0)
+def test_makeImage(monkeypatch):
+    monkeypatch.setattr(np.random, 'rand', lambda: 0.5)
+    monkeypatch.setattr(np.random, 'randn', lambda: 0)
     monkeypatch.setattr(np.random, 'uniform', lambda x, y, z: np.zeros(z))
     a = [OnDetectorPosition(1, 50., 50., 3000.)]
     w = WCS(naxis=2)
@@ -21,11 +24,26 @@ def test_makeImage(monkeypatch) :
     s1.accept(vs)
 
     sut = d1.get_array()
-    assert(len(sut) == 100)
+    assert (len(sut) == 100)
     for x in range(0, 100):
-        assert(len(sut[x]) == 100)
+        assert (len(sut[x]) == 100)
         for y in range(0, 100):
             if (50, 50) == (x, y):
-                assert(sut[x][y] == 3000)
+                assert (sut[x][y] == 3000)
             else:
-                assert(sut[x][y] == 0)
+                assert (sut[x][y] == 0)
+
+
+@pytest.mark.parametrize('pos,randn,actual', [
+    (OnDetectorPosition(1, 50., 50., 3000.), (0.0, 0.0), (50, 50)),
+    (OnDetectorPosition(1, 30., 40., 3000.), (0.0, 0.0), (40, 30)),
+    (OnDetectorPosition(1, 50., 50., 3000.), (0.4, 0.0), (50, 50)),
+    (OnDetectorPosition(1, 50., 50., 3000.), (0.5, 0.0), (51, 50)),
+    (OnDetectorPosition(1, 50., 50., 3000.), (0.0, 0.4), (50, 50)),
+    (OnDetectorPosition(1, 50., 50., 3000.), (0.0, 0.5), (50, 51)),
+])
+def test__incriment_position(mocker: MockFixture, pos, randn, actual):
+    mocker.patch.object(np.random, 'randn').side_effect = randn
+    d1 = DetectorImage("outputa.fits")
+    sut = d1._incriment_position(pos);
+    assert (sut == actual)
