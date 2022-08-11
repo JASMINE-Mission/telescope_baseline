d27798c36eaf91f116210c5b788b6bbe4894875a
Yoshiyuki Yamada <yamada.yoshiyuki@gmail.com>
2022-08-11 17:15:21 +0900

    Remove unnecessary comment

--- src/telescope_baseline/mapping/read_catalog.py
+++ src/telescope_baseline/mapping/read_catalog.py
@@ -35,7 +35,7 @@ def read_jasmine_targets(hdffile):
 
     """
 
-    dat = pd.read_hdf(hdffile) #  hogehoge
+    dat = pd.read_hdf(hdffile)
     ra = dat["ra"].values
     dec = dat["dec"].values
     hw = dat["phot_hw_mag"].values
