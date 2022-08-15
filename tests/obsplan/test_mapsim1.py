2c2962c261c3a37d80b7aaad696f952dcc934497
山根英次 <eijiyamane@gmail.com>
2022-08-15 16:08:33 +0900

    ロジックをmake_imageに押し込む

--- tests/obsplan/test_mapsim1.py
+++ tests/obsplan/test_mapsim1.py
@@ -1,7 +1,8 @@
 import pytest
-from telescope_baseline.obsplan.mapsim1 import degxys
+from telescope_baseline.obsplan.mapsim1 import make_image
 import filecmp
 
 
 def test_it():
+    make_image()
     assert(filecmp.cmp("map.svg", "../tests/obsplan/out.svg"))
