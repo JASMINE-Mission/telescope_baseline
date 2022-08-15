3e8230d3cc52e5557b5584d1043be80b33a1cb9d
山根英次 <eijiyamane@gmail.com>
2022-08-15 16:19:55 +0900

    ファイル無いとエラーになるらしいので、削除前に存在チェックするようにする

--- tests/obsplan/test_mapsim1.py
+++ tests/obsplan/test_mapsim1.py
@@ -4,7 +4,8 @@ import filecmp
 import os
 
 def test_it():
-    os.remove("map.svg")
+    if os.path.isfile("map.svg"):
+            os.remove("map.svg")
     make_image()
     assert(filecmp.cmp("map.svg", "../tests/obsplan/out.svg"))
     os.remove("map.svg")
