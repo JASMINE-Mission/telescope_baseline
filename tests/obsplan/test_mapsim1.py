a9b9a3fdbe56e6878d38d9e9010babd29ee72206
山根英次 <eijiyamane@gmail.com>
2022-08-15 16:12:51 +0900

    テストの前後でファイルを消しておく。最初に消しているのは、テストの途中で止まってしまってゴミが残ってても正しくテスト出来るように。テストの後で消しているのは後始末です。

--- tests/obsplan/test_mapsim1.py
+++ tests/obsplan/test_mapsim1.py
@@ -1,8 +1,11 @@
 import pytest
 from telescope_baseline.obsplan.mapsim1 import make_image
 import filecmp
-
+import os
 
 def test_it():
+    os.remove("map.svg")
     make_image()
     assert(filecmp.cmp("map.svg", "../tests/obsplan/out.svg"))
+    os.remove("map.svg")
+
