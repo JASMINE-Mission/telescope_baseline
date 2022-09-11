b3bd77af5b96f268a2ce0ecf17df7f1989e5576c
山根英次 <eijiyamane@gmail.com>
2022-09-11 16:23:36 +0900

    Response to Codeclimate pointers

--- slowtests/test_slow_test_example.py
+++ slowtests/test_slow_test_example.py
@@ -4,6 +4,5 @@ from test_tools.assert_timeout import assert_timeout
 
 
 def test_slow():
-    slow_func = lambda: time.sleep(5)
-    assert_timeout(10 * 1000, slow_func)    # not error
-    #assert_timeout(4 * 1000, slow_func)    # error
+    assert_timeout(10 * 1000, lambda: time.sleep(5))  # not error
+    # assert_timeout(4 * 1000, lambda: time.sleep(5))    # error
