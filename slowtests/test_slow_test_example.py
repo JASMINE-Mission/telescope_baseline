753ed4aca7ce25598db4db7e8fc35b4b8460c8bf
山根英次 <eijiyamane@gmail.com>
2022-09-16 23:15:53 +0900

    I made it a test to pass.

--- slowtests/test_slow_test_example.py
+++ slowtests/test_slow_test_example.py
@@ -4,5 +4,5 @@ from test_tools.assert_timeout import assert_timeout
 
 
 def test_slow():
-    # assert_timeout(10 * 1000, lambda: time.sleep(5))  # not error
-    assert_timeout(4 * 1000, lambda: time.sleep(5))    # error
+    assert_timeout(10 * 1000, lambda: time.sleep(5))  # not error
+    # assert_timeout(4 * 1000, lambda: time.sleep(5))    # error
