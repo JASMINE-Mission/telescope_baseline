9dafd836cc2b7ea5654a7a75ef576be5fccf0173
Daisuke Kawata <d.kawata@ucl.ac.uk>
2022-09-19 18:25:10 +0100

    The data and test are added for read_catalog.py

--- tests/mapping/test_read_catalog.py
+++ tests/mapping/test_read_catalog.py
@@ -1,9 +1,16 @@
 def test_inout_detector():
-    import pkg_resources           
+    import pkg_resources
     from telescope_baseline.mapping.read_catalog import read_jasmine_targets
-    hdf=pkg_resources.resource_filename('telescope_baseline', 'data/cat.hdf')
-    targets,l,b, hw=read_jasmine_targets(hdf)
+    hdf = pkg_resources.resource_filename('telescope_baseline', 'data/cat.hdf')
+    targets, l, b, hw = read_jasmine_targets(hdf)
     print(len(targets))
-    
-if __name__=="__main__":
+
+    from telescope_baseline.mapping.read_catalog import read_jasmine_targets_jhk
+    hdf_jhk = pkg_resources.resource_filename(
+      'telescope_baseline', 'data/cat_jhk_hw12.5.hdf')
+    targets_jhk, l, b, jmag, hmag, ksmag = read_jasmine_targets_jhk(hdf_jhk)
+    print(len(targets_jhk))
+
+
+if __name__ == "__main__":
     test_inout_detector()
